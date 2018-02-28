6dacc0dfd40c50405862eebcb9bb6e21844a63f7
Kevin Huang <yellow.khaos@gmail.com>
2018-02-28 03:33:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+
+  
+  end
+
+  get '/square/:number' do
+    
+  end
+
 end
