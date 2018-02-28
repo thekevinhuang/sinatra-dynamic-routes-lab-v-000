677e8189ec2f262e5b0c47d1482aa51ad33381bf
Kevin Huang <yellow.khaos@gmail.com>
2018-02-28 03:35:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,10 +13,14 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    
+
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+  end
+
+  get '/:operation/:number1/:number2' do
     
   end
 end
